f1ebbcd67fefc1cb4f498034b897b77d11535f47
Ruchitha Virashan <ruchitha@posvega.com>
2025-09-19 15:21:36 +0530

    init

---
+++ src/main/java/com/moneyminder/MoneyminderApplication.java
@@ -0,0 +1,13 @@
+package com.moneyminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyminderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moneyminder/MoneyminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneyminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
